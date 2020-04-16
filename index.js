11d880d052b9a6b8a27e964d7dc93f670a16222d
Phạm Phong Phú Cường <51190165+phamcuong2751@users.noreply.github.com>
2020-04-16 14:42:05 +0700

    commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let
+
+function mainMenu() {
+    console.log('1. Show list students');
+    console.log('2. Create and additon a new student');
+    console.log('3. Save and exit');
+}
+
+
+
+function createStudent() {
+    mainMenu()
+
+}
+
+function main() {
+    mainMenu()
+
+}
+
+main();
