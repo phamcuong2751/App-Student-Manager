a68006d697786d82cc34577db56afcf0712544f5
Phạm Phong Phú Cường <51190165+phamcuong2751@users.noreply.github.com>
2020-04-16 17:09:35 +0700

    final

--- index.js
+++ index.js
@@ -1,59 +1,65 @@
 let readlineSync = require('readline-sync');
 let fs = require('fs');
 let allStudents = [];
-let student = {};
 
 function loadData() {
-
+    let fileContent = fs.readFileSync('./data.json');
+    allStudents = JSON.parse(fileContent);
 }
 
 function mainMenu() {
     console.log('1. Show list students');
     console.log('2. Create and additon a new student');
     console.log('3. Save and exit');
-}
-
-
-function inputAStuent(student) {
-    let name = readlineSync.question('Name: ');
-    let age = readlineSync.question('Age: ');
-    let sex = readlineSync.question('Sex: ');
-    student.name = name;
-    student.age = age;
-    student.sex = sex;
-    return student;
-}
-
-function showStudents() {
-
-}
-
-function createStudent() {
-
-}
-
-function saveAndExit() {
-
-}
-
-//xoa code nay di ne
-function main() {
-    function loadData()
-    mainMenu()
-    let interchoose = parseInt(readlineSync.question('Nhap lua chon cua ban: '));
+    let interchoose = parseInt(readlineSync.question("Nhap lua chon cua ban: "));
     switch (interchoose) {
         case 1:
             showStudents()
+            mainMenu()
             break;
         case 2:
             createStudent()
+            mainMenu()
             break;
         case 3:
             saveAndExit()
             break;
         default:
+            console.log('Chon sai!');
+            mainMenu();
             break;
     }
+}
+
+
+function showStudents() {
+    for (let studentX of allStudents) {
+        console.log(studentX.name, studentX.age, studentX.sex)
+    }
+}
+
+function createStudent() {
+    let name = readlineSync.question('Name: ');
+    let age = readlineSync.question('Age: ');
+    let sex = readlineSync.question('Sex: ');
+    student = {
+        name: name,
+        age: parseInt(age),
+        sex: sex
+    }
+    allStudents.push(student);
+}
+
+function saveAndExit() {
+    let content = JSON.stringify(allStudents);
+    fs.writeFileSync('./data.json', content, { encoding: 'utf8' });
+}
+
+function main() {
+    loadData();
+    console.log(allStudents);
+    mainMenu()
+
 }
 
 main();
