1ff376248c2043c57021c146c05e89c66799ba2d
Phạm Phong Phú Cường <51190165+phamcuong2751@users.noreply.github.com>
2020-04-16 14:47:43 +0700

    commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-let
+let readlineSync = require('readline-sync');
+let allStudents = [];
 
 function mainMenu() {
     console.log('1. Show list students');
     console.log('2. Create and additon a new student');
     console.log('3. Save and exit');
 }
+
 
 
 
