f5e0bcb56cf7f6531a2037a971535165ffba983a
Phạm Phong Phú Cường <51190165+phamcuong2751@users.noreply.github.com>
2020-04-16 15:06:09 +0700

    chuc năng

--- index.js
+++ index.js
@@ -7,8 +7,33 @@ function mainMenu() {
     console.log('3. Save and exit');
 }
 
+function inputAStuent(student) {
+    let name = readlineSync.question('Name: ');
+    let age = readlineSync.question('Age: ');
+    let sex = readlineSync.question('Sex: ');
+    student.name = name;
+    student.age = age;
+    student.sex = sex;
+    return student;
+}
+
 function createStudent() {
-    mainMenu()
+    do {
+        mainMenu()
+        let interchoose = readlineSync.question('Nhập lựa chọn của bạn: ')
+        interchoose;
+        switch (interchoose) {
+            case 1:
+                console.log(allStudents);
+                break;
+            case 2:
+                allStudents.unshift(inputAStuent(student));
+                break;
+            case 3:
+
+                break;
+        }
+    } while (interchoose < 4);
 
 }
 //xoa code nay di ne
